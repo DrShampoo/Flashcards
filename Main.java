b5e2893b6e4501b37506149235954d413de108a3
Alexander Mozharovskij <marrruska0@gmail.com>
2020-03-24 18:54:11 +0300

    Add Main

--- Main.java
+++ Main.java
@@ -4,11 +4,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String term = scanner.nextLine();
-        String definition = scanner.nextLine();
-        String answer = scanner.nextLine();
-        if (answer.equals(definition)) System.out.println("right");
-        else System.out.println("wrong");
+        System.out.println("Input the number of cards");
+        Scanner scanner = new Scanner(System.in).useDelimiter("\\n");
+        int n = scanner.nextInt();
+        String s = scanner.nextLine();
+        String[] card = new String[n];
+        String[] cardDef = new String[n];
+        for (int i = 0; i < n; i++){
+            System.out.println("The card #" + (i+1) +":");
+            card[i] = scanner.nextLine();
+            System.out.println("The definition of the card #" + (i+1) + ":");
+            cardDef[i] = scanner.nextLine();
+        }
+        for (int i = 0; i < n; i++){
+            System.out.println("Print the definition of \"" + card[i] + "\":");
+            String str = scanner.nextLine();
+            if (str.equals(cardDef[i]))
+                System.out.println("Correct answer");
+            else
+                System.out.println("Wrong answer. The correct one is \"" + cardDef[i] + "\".");
+        }
+
+
     }
 }
